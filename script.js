a9dfba0050332bb4f78ea9a2620107b1e2dc4bb0
TheCodingPanda1 <Zander.Weeks@education.nsw.gov.au>
2026-01-03 19:26:10 +0000

    Added resizeSVG function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+//Resizing svgs
+function resizeSVG () {
+    var svgs = document.getElementsByClassName("resizable");
+    var div = document.createElement("div");
+    div.style.width = "1px";
+    document.body.appendChild(div);
+    if(div.clientWidth == 16){
+        return;
+    } else {
+        var width = div.clientWidth;
+        for(let svg in svgs){
+            svg.style.scale = width / 16;
+        }
+    }
+}
