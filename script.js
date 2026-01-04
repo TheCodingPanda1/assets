0fb603482e2c885c1a24146a372e554be90ffca2
TheCodingPanda1 <Zander.Weeks@education.nsw.gov.au>
2026-01-04 23:41:18 +0000

    Fixed not working bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function resizeSVG () {
     var svgs = document.getElementsByClassName("resizable");
     var div = document.createElement("div");
-    div.style.width = "1px";
+    div.style.width = "1rem";
     document.body.appendChild(div);
     if(div.clientWidth == 16){
         return;
